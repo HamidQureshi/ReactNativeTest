43878e60f61ed278a8832bc9d5a36ef59eb9fe66
Hamid Mehmood <hamidqureshi.hq000@gmail.com>
2019-08-23 16:54:03 +0100

    nav bar changes

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-import { createStackNavigator, createAppContainer, createBottomTabNavigator } from 'react-navigation';
+import { createStackNavigator, createAppContainer, createBottomTabNavigator, createMaterialTopTabNavigator } from 'react-navigation';
 import HomeScreen from './screens/HomeScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import TabScreen from './screens/TabScreen'
 import { Ionicons } from '@expo/vector-icons';
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, Button, TextInput } from 'react-native';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+
 
 
 const Stack1 = createStackNavigator(
   {
-    Home: {screen :HomeScreen},
+    Home: { screen: HomeScreen },
     Profile: ProfileScreen,
   },
   {
@@ -19,31 +21,61 @@ const Stack1 = createStackNavigator(
 
 const Stack2 = createStackNavigator(
   {
-    Home: {screen :HomeScreen},
-    Tab: TabScreen 
+    Home: { screen: HomeScreen },
+    Tab: TabScreen
   },
   {
     initialRouteName: "Tab"
   }
 );
 
-const TabNavigator = createBottomTabNavigator({  
-  Tab1Header: 
-  {screen :Stack1,
-    navigationOptions:{
+const TabNavigator = createMaterialBottomTabNavigator({
+  // const TabNavigator = createMaterialTopTabNavigator({
+  Tab1Header:
+  {
+    screen: Stack1,
+    navigationOptions: {
       tabBarLabel: "Tab1",
-      tabBarIcon:({tintColor})=>(<Ionicons name="ios-home" color={tintColor} size={25}/>)
-    }},  
-  Tab2Header: 
-  {screen :Stack2,
-    navigationOptions:{
+      tabBarIcon: ({ tintColor }) => (
+        <View>
+          <Ionicons name="ios-home" color={tintColor} size={25} />
+        </View>
+      ),
+      activeColor: '#f60c0d',
+      inactiveColor: '#f65a22',
+      barStyle: { backgroundColor: '#f69b31' },
+    }
+  },
+  Tab2Header:
+  {
+    screen: Stack2,
+    navigationOptions: {
       tabBarLabel: "Tab2",
-      tabBarIcon:({tintColor})=>(<Ionicons name="ios-person" color={tintColor} size={25}/>)
-    }},  
+      tabBarIcon: ({ tintColor }) => (
+        <View>
+          <Ionicons name="ios-person" color={tintColor} size={25} />
+        </View>
+      ),
+      activeColor: '#615af6',
+      inactiveColor: '#46f6d7',
+      barStyle: { backgroundColor: '#67baf6' },
+    }
+  },
 },
+  //top nav bar settings
+  // {
+  //   tabBarOptions: {
+  //     activeTintColor: 'white',
+  //     showIcon: true,
+  //     showLabel: false,
+  //     style: {
+  //       backgroundColor: 'red'
+  //     }
+  //   },
+  // },
   {
     initialRouteName: "Tab2Header"
-  }); 
+  });
 
 export default createAppContainer(TabNavigator);
 
--- screens/TabScreen.js
+++ screens/TabScreen.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, Button, TextInput } from 'react-native';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';  
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 export default class TabScreen extends Component {
 
     render() {
         return (
             <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                <Text>Tabq Screen</Text>         
+                <Text>Tabd Screen</Text>
             </View>
         );
     }
